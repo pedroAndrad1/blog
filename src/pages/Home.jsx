72012d6c8ca7d683d559fbcc49396af393ae212f
Pedro <pedropaulo3521@gmail.com>
2020-08-18 20:56:40 -0300

    Adicionada api server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Grid, makeStyles, Divider, Typography } from '@material-ui/core';
 import imagem from '../images/img.jpg'
 import MainPost from '../components/mainPost/MainPost';
 import SecondaryPost from '../components/secondary-post/SecondaryPost';
-import { deepPurple, purple } from '@material-ui/core/colors';
+import { deepPurple } from '@material-ui/core/colors';
 import Topicos from '../components/trends-carousel/TrendsCarousel';
 import Transition from '../utils/transition/Transition';
 import PostList from '../components/posts-list/PostsList';
+import { useEffect } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
-//Post em destaque
+/*//Post em destaque
 const mainPost = {
       title: 'Título do Post',
       description:
@@ -179,7 +181,7 @@ const topicos = [
             imgText: 'main image description',
             linkText: 'Leia mais…',
       },
-]
+] */
 
 const useStyles = makeStyles(theme => ({
 
@@ -192,55 +194,101 @@ const useStyles = makeStyles(theme => ({
             marginBottom: theme.spacing(3),
       },
 
-}))
+})) 
+
+const URL_BACKEND = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080'
+  : '';
+
 
 const Home = () => {
 
+      const [mainPost, setMainPost] = useState();
+      const [secundaryPosts, setSecundaryPosts] = useState();
+      const [trends, setTrends] = useState();
+      const [posts, setPosts] = useState();
+
+      useEffect(() => {
+            fetch(`${URL_BACKEND}/main-post`)
+                  .then(res =>  res.json())
+                  .then(res => setMainPost(res));
+
+            fetch(`${URL_BACKEND}/secundary-posts`)
+                  .then(res => res.json())
+                  .then(res => setSecundaryPosts(res));
+
+            fetch(`${URL_BACKEND}/trends`)
+                  .then(res => res.json())
+                  .then(res => setTrends(res));
+
+            fetch(`${URL_BACKEND}/posts`)
+                  .then(res => res.json())
+                  .then(res => setPosts(res));
+
+      }, [])
+
+
+      console.log(mainPost, secundaryPosts, trends, posts)
+
       const classes = useStyles()
-
       return (
-            /*Slide com true no atributo in faz com que a pagina tenha o efeito de slide 
-            quando renderizada junto com o tabs */
-            <Transition transition='slide' direction="right" in={true} timeout={500}>
-                  <div>
-                        {/*MaxWidth de 1232px, contando com as margin laterais do container. Sem elas, e 1260px*/}
-                        <Container component='section' maxWidth='lg' className={classes.container}  >
-
-                              <MainPost post={mainPost}></MainPost>
-                              <Divider variant='middle' />
-
-                              {/** Grid container para os posts secundarios */}
-                              <Grid container spacing={5} className={classes.container}>
-                                    {secundaryPosts.map((post, i) => (
-                                          <Grid item xs={12} md={6}>
-                                                {/**O card vai ocupar metade do grid em telas de maiores e em telas 
-                                                 * de 600px para baixo vai ocupar a linha inteira
-                                                 */}
-                                                <SecondaryPost key={i} post={post} />
-                                          </Grid>
-                                    ))}
+            <>    
+                  {
+                        (!mainPost || !secundaryPosts  || !trends || !posts) &&     
+                              <Grid container justify='center'>
+                                    <CircularProgress size={175} />
                               </Grid>
-                        </Container>
+                  }
+                  {     
+                        //A pagina so e exibida quando recebemos as infos do servidor
+                        (mainPost && secundaryPosts  && trends && posts) &&
+                        
+                        /*Slide com true no atributo in faz com que a pagina tenha o efeito de slide 
+                        quando renderizada junto com o tabs */
+                        <Transition transition='slide' direction="right" in={true} timeout={500}>
+                              <div>
+                                    {/*MaxWidth de 1232px, contando com as margin laterais do container. Sem elas, e 
+                                    1260px*/}
+                                    <Container component='section' maxWidth='lg' className={classes.container}  >
 
-                        <section className={classes.containerTopicos}>
-                              <Topicos topicos={topicos} />
-                        </section>
+                                          <MainPost post={mainPost}></MainPost>
+                                          <Divider variant='middle' />
 
-                        <Container
-                              component='section'
-                              maxWidth='md'
-                              className={classes.container}
-                              style={{ marginBottom: 30 }}>
-                              <Typography align='center' variant='h4' component='h2' style={{ opacity: 0.6,}} gutterBottom>
-                                    Outros posts                       
-                              </Typography>
-                              <PostList posts={posts} />
-                              
-                        </Container>
+                                          {/** Grid container para os posts secundarios */}
+                                          <Grid container spacing={5} className={classes.container}>
+                                                {secundaryPosts.map((post, i) => (
+                                                      <Grid item xs={12} md={6}>
+                                                            {/**O card vai ocupar metade do grid em telas de maiores e
+                                                             * em telas 
+                                                            * de 600px para baixo vai ocupar a linha inteira
+                                                 */}
+                                                            <SecondaryPost key={i} post={post} />
+                                                      </Grid>
+                                                ))}
+                                          </Grid>
+                                    </Container>
 
-                  </div>
-            </Transition>
+                                    <section className={classes.containerTopicos}>
+                                          <Topicos topicos={trends} />
+                                    </section>
 
+                                    <Container
+                                          component='section'
+                                          maxWidth='md'
+                                          className={classes.container}
+                                          style={{ marginBottom: 30 }}>
+                                          <Typography align='center' variant='h4' component='h2' style={{ opacity: 0.6, }} >
+                                                Outros posts
+                                           </Typography>
+                                          <Divider variant='middle' style={{ marginBottom: '30px' }} />
+                                          <PostList posts={posts} />
+
+                                    </Container>
+
+                              </div>
+                        </Transition>
+                  }
+            </>
       );
 }
 export default Home;
